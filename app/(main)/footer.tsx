b5f02392233754b56cd09723f73de3ebad6ca879
hamster1963 <1410514192@qq.com>
2025-01-02 18:10:40 +0800

    fix: footer

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -1,7 +1,4 @@
-import pack from "@/package.json"
-
 export default function Footer() {
-  const version = pack.version
   return (
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
@@ -14,13 +11,9 @@ export default function Footer() {
           >
             {"GitHub"}
           </a>
-          <a
-            href={`https://github.com/hamster1963/nezha-dash/releases/tag/v${version}`}
-            target="_blank"
-            className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-600 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-500/60 dark:hover:decoration-yellow-500/80"
-          >
-            v{version}
-          </a>
+          <p className="cursor-pointer font-normal underline decoration-yellow-500 hover:decoration-yellow-600 transition-colors decoration-2 underline-offset-2 dark:decoration-yellow-500/60 dark:hover:decoration-yellow-500/80">
+            V2-SSE
+          </p>
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {"© 2020-"}
