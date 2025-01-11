265950e40f1993e39be501297a1d82eaa1852fd8
hamster1963 <1410514192@qq.com>
2025-01-11 20:04:56 +0800

    feat: sync

--- app/(main)/ClientComponents/detail/ServerDetailClient.tsx
+++ app/(main)/ClientComponents/detail/ServerDetailClient.tsx
@@ -50,7 +50,7 @@ export default function ServerDetailClient({ server_id }: { server_id: number })
     <div>
       <div
         onClick={linkClick}
-        className="flex flex-none cursor-pointer font-semibold leading-none items-center break-all tracking-tight gap-0.5 text-xl"
+        className="flex flex-none cursor-pointer font-semibold leading-none items-center break-all tracking-tight gap-0.5 text-xl hover:opacity-50 transition-opacity duration-300"
       >
         <BackIcon />
         {data?.name}
